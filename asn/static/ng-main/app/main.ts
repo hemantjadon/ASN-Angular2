16985eec606d5e450861d23fa1056d35b718ec3e
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-06-30 09:35:14 +0530

    (refactor) : switching to polymer

--- asn/static/ng-main/app/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { AUTH_PROVIDERS } from 'angular2-jwt';
-
-import { AppComponent } from './packages/app.component';
-
-bootstrap(AppComponent,[ HTTP_PROVIDERS , AUTH_PROVIDERS ]);
--- asn/static/ng-main/app/packages/home-page/home-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'home-page',
-	templateUrl: "static/ng-main/build/app/templates/home-page.component.html",
-	styleUrls: ["static/ng-main/build/app/styles/home-page.component.css"]
-})
-export class HomePageComponent{}
